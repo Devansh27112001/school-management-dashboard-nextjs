be3660cee20474d86a3022b12e83eeabd9fc02a4
Devansh27112001 <dkansara2711@gmail.com>
2024-10-27 14:59:02 -0400

    CLIENT: building the navbar

--- src/app/dashboard/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function dashboardLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <div>dashboard{children}</div>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="text-md">Home page</div>;
+  return <div>Home page</div>;
 }
