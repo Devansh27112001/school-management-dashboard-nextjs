baf9692bede5c63125ed867fff97ef5597499c80
Devansh27112001 <dkansara2711@gmail.com>
2024-10-29 14:09:12 -0400

    CLEINT: working on the navbar

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,14 +4,20 @@ import Link from "next/link";
 
 const Menu = () => {
   return (
-    <div className="">
+    <div className="mt-4 text-sm">
       {menuItems.map((i) => (
-        <div className="" key={i.title}>
-          <span className="">{i.title}</span>
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-2">
+            {i.title}
+          </span>
           {i.items?.map((item) => (
-            <Link key={item.label} href={item.href}>
+            <Link
+              key={item.label}
+              href={item.href}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
               <Image src={item.icon} width={20} height={20} alt="" />
-              <span>{item.label}</span>
+              <span className="hidden lg:block">{item.label}</span>
             </Link>
           ))}
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between p-4">
+      {/* SEARCH BAR */}
+      <div className="hidden md:flex">
+        <Image src={"/search.png"} width={14} height={14} alt="" />
+        <input type="text" placeholder="Search..." />
+      </div>
+      {/* ICONS and USERS */}
+      <div className="">
+        <div className=""></div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
