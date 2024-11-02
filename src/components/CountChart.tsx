c891042054548c24c548f04b60af863ea2db049e
Devansh27112001 <dkansara2711@gmail.com>
2024-11-02 13:43:12 -0400

    CLIENT: completed the radial chart

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -1,57 +1,25 @@
 "use client";
 import Image from "next/image";
-import {
-  RadialBarChart,
-  RadialBar,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+import { RadialBarChart, RadialBar, ResponsiveContainer } from "recharts";
 
 const CountChart = () => {
   const data = [
     {
-      name: "18-24",
-      uv: 31.47,
-      pv: 2400,
-      fill: "#8884d8",
+      name: "Total",
+      count: 106,
+      fill: "white",
     },
     {
-      name: "25-29",
-      uv: 26.69,
-      pv: 4567,
-      fill: "#83a6ed",
+      name: "Girls",
+      count: 53,
+      fill: "#FAE27C",
     },
     {
-      name: "30-34",
-      uv: 15.69,
-      pv: 1398,
-      fill: "#8dd1e1",
-    },
-    {
-      name: "35-39",
-      uv: 8.22,
-      pv: 9800,
-      fill: "#82ca9d",
-    },
-    {
-      name: "40-49",
-      uv: 8.63,
-      pv: 3908,
-      fill: "#a4de6c",
-    },
-    {
-      name: "50+",
-      uv: 2.63,
-      pv: 4800,
-      fill: "#d0ed57",
+      name: "Boys",
+      count: 53,
+      fill: "#CFCEFF",
     },
   ];
-  const style = {
-    top: "50%",
-    right: 0,
-    transform: "translate(0, -50%)",
-    lineHeight: "24px",
-  };
 
   return (
     <div className="bg-white rounded-xl w-full h-full pt-2 px-2">
@@ -62,31 +30,30 @@ const CountChart = () => {
       </div>
       {/* CHART */}
 
-      <div>
+      <div className={"relative"}>
         <ResponsiveContainer width="100%" height="100%">
           <RadialBarChart
-            width={300}
-            height={300}
+            width={200}
+            height={200}
             cx="50%"
             cy="50%"
-            innerRadius="10%"
-            outerRadius="80%"
-            barSize={14}
+            innerRadius="40%"
+            outerRadius="100%"
+            barSize={32}
             data={data}
           >
-            <RadialBar
-              label={{ position: "insideStart", fill: "#fff" }}
-              background
-              dataKey="uv"
-            />
-            {/* <Legend
-              iconSize={10}
-              layout="vertical"
-              verticalAlign="middle"
-              wrapperStyle={style}
-            /> */}
+            <RadialBar background dataKey="count" />
           </RadialBarChart>
         </ResponsiveContainer>
+        <Image
+          src={"/maleFemale.png"}
+          alt=""
+          height={50}
+          width={50}
+          className={
+            "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          }
+        />
       </div>
 
       {/* BOTTOM */}
