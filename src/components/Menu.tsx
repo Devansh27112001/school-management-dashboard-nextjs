0d0eded295804540180c2b853d27ea9b78e1b719
Devansh27112001 <dkansara2711@gmail.com>
2024-11-08 14:34:15 -0500

    CLIENT:admin page implemenetd

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { menuItems } from "@/lib/data";
+import { menuItems, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -10,16 +10,19 @@ const Menu = () => {
           <span className="hidden lg:block text-gray-400 font-light my-2">
             {i.title}
           </span>
-          {i.items?.map((item) => (
-            <Link
-              key={item.label}
-              href={item.href}
-              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
-            >
-              <Image src={item.icon} width={20} height={20} alt="" />
-              <span className="hidden lg:block">{item.label}</span>
-            </Link>
-          ))}
+          {i.items?.map(
+            (item) =>
+              item.visible.includes(role) && (
+                <Link
+                  key={item.label}
+                  href={item.href}
+                  className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2 md:px-2 rounded-md hover:bg-devanshSkyLight transition-all duration-100 ease-in"
+                >
+                  <Image src={item.icon} width={20} height={20} alt="" />
+                  <span className="hidden lg:block">{item.label}</span>
+                </Link>
+              )
+          )}
         </div>
       ))}
     </div>
