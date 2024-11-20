c8ec033815f3562e7ad6db794b47d82a04fe7272
Devansh27112001 <dkansara2711@gmail.com>
2024-11-20 10:34:31 -0500

    CLIENT: working on the reusable modal component

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import Image from "next/image";
 import { useState } from "react";
 
@@ -31,6 +30,23 @@ const FormModal = ({ table, type, data, id }: FormModalProps) => {
       : type === "update"
       ? "bg-devanshSky"
       : "bg-devanshPurple";
+
+  const Form = () => {
+    return type === "delete" && id ? (
+      <form action="" className="p-4 flex flex-col gap-4">
+        <span className="text-center font-medium">
+          All data will be lost. Are you sure you want to delete this {table}{" "}
+          record?
+        </span>
+        <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none">
+          Delete
+        </button>
+      </form>
+    ) : (
+      "Create or update record"
+    );
+  };
+
   return (
     <>
       <button
@@ -43,7 +59,7 @@ const FormModal = ({ table, type, data, id }: FormModalProps) => {
         // WRAPPER
         <div className="w-screen h-screen absolute top-0 left-0 bg-black bg-opacity-65 z-50 flex items-center justify-center">
           <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[50%]">
-            <h1 className="text-2xl font-semibold">{type}</h1>
+            <Form />
             <div
               className="absolute top-2 right-2 cursor-pointer"
               onClick={() => setOpen(false)}
