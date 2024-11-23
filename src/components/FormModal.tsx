aa89e16bc894f43fe77ca4b3c0c6d04823688fd7
Devansh27112001 <dkansara2711@gmail.com>
2024-11-23 11:49:51 -0500

    CLIENT: implementing lazy loading for the forms

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,8 +1,18 @@
 "use client";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
-import TeacherForm from "./forms/TeacherForm";
-import StudentForm from "./forms/StudentForm";
+import LoadingSpinner from "./LoadingSpinner";
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <LoadingSpinner />,
+});
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <LoadingSpinner />,
+});
+
 type FormObject = Record<
   string,
   (type: "create" | "update", data?: any) => JSX.Element
