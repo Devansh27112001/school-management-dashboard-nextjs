e70154c9564f59c7b6ad6886b7c397ce256f5899
Devansh27112001 <dkansara2711@gmail.com>
2025-01-06 17:36:13 -0500

    FRONTEND: ParentForm implemented

--- src/components/forms/ParentForm.tsx
+++ src/components/forms/ParentForm.tsx
@@ -3,6 +3,8 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import InputField from "../InputField";
+import Image from "next/image";
 
 const schema = z.object({
   username: z
@@ -41,9 +43,131 @@ const ParentForm = ({ type, data }: ParentFormProps) => {
     handleSubmit,
     formState: { errors },
   } = useForm<Inputs>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: any) => {
+    alert("Form submitted successfully");
+  };
   return (
-    <form className="flex flex-col gap-8">
+    <form className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
       <h1 className="">Create a Parent</h1>
+      <span className="text-xs text-gray-400 font-medium">
+        Authentication information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="Username"
+          error={errors?.username}
+          name="username"
+          register={register}
+          defaultValue={data?.username}
+        />
+        <InputField
+          label="Email"
+          type="email"
+          error={errors?.email}
+          name="email"
+          register={register}
+          defaultValue={data?.email}
+        />
+        <InputField
+          label="Password"
+          name="password"
+          type="password"
+          error={errors?.password}
+          register={register}
+          defaultValue={data?.password}
+        />
+      </div>
+
+      <span className="text-xs text-gray-400 font-medium">
+        Personal information
+      </span>
+      <div className="flex justify-between flex-wrap gap-4">
+        <InputField
+          label="First name"
+          error={errors?.firstName}
+          register={register}
+          name="firstName"
+          defaultValue={data?.firstName}
+        />
+        <InputField
+          label="Last name"
+          error={errors?.lastName}
+          register={register}
+          name="lastName"
+          defaultValue={data?.lastName}
+        />
+        <InputField
+          label="Phone"
+          error={errors?.phone}
+          register={register}
+          name="phone"
+          defaultValue={data?.phone}
+        />
+        <InputField
+          label="Address"
+          error={errors?.address}
+          register={register}
+          name="address"
+          defaultValue={data?.address}
+        />
+        <InputField
+          label="Blood Type"
+          error={errors?.bloodType}
+          register={register}
+          name="bloodType"
+          defaultValue={data?.bloodType}
+        />
+        <InputField
+          label="Birthday"
+          error={errors?.birthday}
+          register={register}
+          name="birthday"
+          defaultValue={data?.birthday}
+          type="date"
+        />
+
+        {/* GENDER SELECT ELEMENT */}
+        <div className="flex flex-col gap-2 w-full md:w-1/4 group">
+          <label className="text-xs text-gray-500 group-focus-within:font-semibold transition-all duration-300">
+            Sex
+          </label>
+          <select
+            {...register("sex")}
+            defaultValue={data?.sex}
+            className="ring-[1.5px] ring-gray-300 p-2 rounded-md w-full focus:ring-blue-300 outline-none transition-all duration-300 text-sm"
+          >
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+          {errors?.sex?.message && (
+            <p className="text-xs text-red-400">
+              {errors?.sex?.message.toString()}
+            </p>
+          )}
+        </div>
+
+        {/* UPLOAD PHOTO ELEMENT*/}
+        <div className="flex flex-col gap-2 w-full md:w-[63%] md:mt-5 self-center justify-start">
+          <label
+            className="text-xs text-gray-500 flex items-center gap-2 cursor-pointer font-semibold"
+            htmlFor="image"
+          >
+            <Image src={"/upload.png"} width={24} height={24} alt="" />
+            <span>Upload photo</span>
+          </label>
+          <input
+            type="file"
+            {...register("img")}
+            className="hidden"
+            id="image"
+          />
+          {errors?.img?.message && (
+            <p className="text-xs text-red-400">{errors.img.message}</p>
+          )}
+        </div>
+      </div>
     </form>
   );
 };
