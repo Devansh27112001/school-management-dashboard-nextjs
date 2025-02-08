94bc588a8e0d5b66491c63761ee32a5681df4e6f
Devansh27112001 <dkansara2711@gmail.com>
2025-02-07 21:53:31 -0500

    Adding types for reusability

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,48 @@
-import { Announcement, Class } from "@prisma/client";
+import {
+  Announcement,
+  Assignment,
+  Class,
+  Exam,
+  Lesson,
+  Parent,
+  Student,
+  Subject,
+  Teacher,
+} from "@prisma/client";
 
 export type searchParamsType = Promise<{
   [key: string]: string | undefined;
 }>;
 
 export type AnnouncementList = Announcement & { class: Class };
+export type AssignmentsList = Assignment & {
+  lesson: { subject: Subject; teacher: Teacher; class: Class };
+};
+export type ClassesList = Class & { supervisor: Teacher };
+export type EventsList = Event & { class: Class };
+export type ExamsList = Exam & {
+  lesson: { subject: Subject; teacher: Teacher; class: Class };
+};
+
+export type LessonsList = Lesson & {
+  class: Class;
+  teacher: Teacher;
+  subject: Subject;
+};
+
+export type ParentsList = Parent & { students: Student[] };
+export type ResultsList = {
+  id: number;
+  title: string;
+  studentName: string;
+  studentSurname: string;
+  teacherName: string;
+  teacherSurname: string;
+  score: number;
+  className: string;
+  startTime: Date;
+};
+
+export type StudentsList = Student & { Class: Class };
+export type SubjectsList = Subject & { teacher: Teacher[] };
+export type TeachersList = Teacher & { subjects: Subject[]; classes: Class[] };
